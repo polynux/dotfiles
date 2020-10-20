c534572ba55a10bf3133f228d5fb834cbea4fd90
Guillaume Dorce <guillaume.dorce@epita.fr>
2020-10-20 20:46:03 +0200

    fix plugins install

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ cp -f init.toml ~/.SpaceVim.d/
 cp -f zshrc ~/.zshrc
 
 # zsh plugins
+export ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
