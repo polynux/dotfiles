f5a084a6ce45615e33775db8f2a034fe61c9f015
guillaume.dorce <guillaume.dorce@bm-services.com>
2022-06-22 10:13:47 +0200

    fix null-ls

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,39 +1,39 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 local b = null_ls.builtins
 
 local sources = {
 
-   b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "scss", "javascript", "json" } },
+    b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "scss", "javascript", "json" } },
 
-   b.formatting.phpcsfixer,
-    b.diagnostics.stylelint.with {filetypes = { "css", "scss", "sass" }},
+    b.formatting.phpcsfixer,
+    b.diagnostics.stylelint.with { filetypes = { "css", "scss", "sass" } },
 
-   -- Lua
-   b.formatting.stylua,
-   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+    -- Lua
+    b.formatting.stylua,
+    b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
 
     b.diagnostics.golangci_lint,
     -- b.formatting.golangci_lint,
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+    -- Shell
+    b.formatting.shfmt,
+    b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
 }
 
 local M = {}
 
 M.setup = function()
-   null_ls.setup {
-      debug = true,
-      sources = sources,
+    null_ls.setup {
+        debug = true,
+        sources = sources,
 
-      -- format on save
-      on_attach = function(client)
-         if client.server_capabilities.document_formatting then
-            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.format({async = true})"
-         end
-      end,
-   }
+        -- format on save
+        on_attach = function(client)
+            if client.server_capabilities.document_formatting then
+                vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.format({async = true})"
+            end
+        end,
+    }
 end
 
 return M
