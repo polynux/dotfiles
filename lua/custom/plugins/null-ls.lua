f282b7d78080bee82436e4b57458df1220b97534
Guillaume Dorce <polynux.dev@gmail.com>
2022-06-22 23:10:53 +0200

    null ls format on save

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,39 +1,47 @@
 local null_ls = require("null-ls")
 local b = null_ls.builtins
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
 
-    b.formatting.prettierd.with { filetypes = { "html", "markdown", "css", "scss", "javascript", "json" } },
+	b.formatting.prettierd.with({ filetypes = { "html", "markdown", "css", "scss", "javascript", "json" } }),
 
-    b.formatting.phpcsfixer,
-    b.diagnostics.stylelint.with { filetypes = { "css", "scss", "sass" } },
+	b.formatting.phpcsfixer,
+	b.diagnostics.stylelint.with({ filetypes = { "css", "scss", "sass" } }),
 
-    -- Lua
-    b.formatting.stylua,
-    b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+	-- Lua
+	b.formatting.stylua,
+	b.diagnostics.luacheck.with({ extra_args = { "--global vim" } }),
 
-    b.diagnostics.golangci_lint,
-    -- b.formatting.golangci_lint,
+	b.diagnostics.golangci_lint,
+	-- b.formatting.golangci_lint,
 
-    -- Shell
-    b.formatting.shfmt,
-    b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
 local M = {}
 
 M.setup = function()
-    null_ls.setup {
-        debug = true,
-        sources = sources,
+	null_ls.setup({
+		debug = true,
+		sources = sources,
 
-        -- format on save
-        on_attach = function(client)
-            if client.server_capabilities.document_formatting then
-                vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.format({async = true})"
-            end
-        end,
-    }
+		-- format on save
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.formatting_sync()
+					end,
+				})
+			end
+		end,
+	})
 end
 
 return M
