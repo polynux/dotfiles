7285d6689e116e91f0899cdf812d178ca00da63f
Guillaume Dorce <polynux.dev@gmail.com>
2022-06-24 20:18:14 +0200

    nvim yuck

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,12 +1,13 @@
 return {
-    ["p00f/nvim-ts-rainbow"] = {},
-    ["jose-elias-alvarez/null-ls.nvim"] = {
-        after = "nvim-lspconfig",
-        config = function()
-            require("custom.plugins.null-ls").setup()
-        end,
-    },
-    ["blueyed/smarty.vim"] = {},
-    ["alker0/chezmoi.vim"] = {},
-    ["dag/vim-fish"] = {},
+	["p00f/nvim-ts-rainbow"] = {},
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls").setup()
+		end,
+	},
+	["blueyed/smarty.vim"] = {},
+	["alker0/chezmoi.vim"] = {},
+	["dag/vim-fish"] = {},
+	["elkowar/yuck.vim"] = {},
 }
