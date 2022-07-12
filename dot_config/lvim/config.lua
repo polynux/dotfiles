c6a9e421b380ca7772a0336859b602d3d2344890
guillaume.dorce <guillaume.dorce@bm-services.com>
2022-07-12 16:43:18 +0200

    disable minimap

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -172,8 +172,8 @@ lvim.plugins = {
         -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
         config = function()
             vim.cmd("let g:minimap_width = 10")
-            -- vim.cmd("let g:minimap_auto_start = 1")
-            -- vim.cmd("let g:minimap_auto_start_win_enter = 1")
+            vim.cmd("let g:minimap_auto_start = 0")
+            vim.cmd("let g:minimap_auto_start_win_enter = 0")
         end,
     },
     {
