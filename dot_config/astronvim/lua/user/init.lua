e1edfdc7601ab6e899fa27847141c00fddebdd57
Guillaume Dorce <polynux.dev@gmail.com>
2023-01-10 16:28:58 +0100

    configure copilot ans smarty

--- dot_config/astronvim/lua/user/init.lua
+++ dot_config/astronvim/lua/user/init.lua
@@ -1,16 +1,46 @@
 local config = {
-  plugins = {
-    init = {
-      ["github/copilot.vim"] = {},
-      ["catppuccin/nvim"] = { as = "catppuccin" },
-      ["nelsyeung/twig.vim"] = {},
+    plugins = {
+        init = {
+            {
+                "zbirenbaum/copilot.lua",
+                cmd = "Copilot",
+                event = { "VimEnter" },
+                config = function ()
+                    vim.defer_fn(function()
+                        require("copilot").setup({
+                            suggestion = { enabled = false },
+                            panel = { enabled = false },
+                        })
+                    end, 100)
+                end,
+            },
+            {
+                "zbirenbaum/copilot-cmp",
+                after = { "copilot.lua" },
+                config = function()
+                    require("copilot_cmp").setup()
+                end
+            },
+            ["catppuccin/nvim"] = { as = "catppuccin" },
+            ["nelsyeung/twig.vim"] = {},
+            ["blueyed/smarty.vim"] = {},
+        }
+    },
+    lsp = {
+        formatting = {
+            format_on_save = false,
+        }
+    },
+    options = {
+        opt = {
+            wrap = true,
+            textwidth = 90,
+        }
+    },
+    mappings = {
+        i = {
+        },
     }
-  },
-  lsp = {
-    formatting = {
-      format_on_save = false,
-    }
-  }
 }
 
 return config
