e58609485a5182c6af1ba286c0584fc6d76be9f6
Guillaume Dorce <polynux.dev@gmail.com>
2023-01-19 13:52:05 +0100

    fix copilot in neovim

--- dot_config/astronvim/lua/user/init.lua
+++ dot_config/astronvim/lua/user/init.lua
@@ -4,41 +4,39 @@ local config = {
             ["zbirenbaum/copilot.lua"] = {
                 cmd = "Copilot",
                 event = { "VimEnter" },
-                config = function ()
+                config = function()
                     vim.defer_fn(function()
                         require("copilot").setup({
-                            suggestion = { enabled = false },
+                            suggestion = { enabled = true, auto_trigger = true },
                             panel = { enabled = false },
+                            -- suggestion = { enabled = false },
                         })
                     end, 100)
-                end,
+                end
             },
             ["zbirenbaum/copilot-cmp"] = {
-                after = { "copilot.lua" },
+                after = { "copilot.lua", "nvim-cmp" },
                 config = function()
-                    require("copilot_cmp").setup()
-                    astronvim.add_user_cmp_source "copilot"
+                    require("copilot_cmp").setup({
+                        method = "getPanelCompletions",
+                    })
+                    astronvim.add_user_cmp_source ("copilot", 800)
                 end
             },
             ["catppuccin/nvim"] = { as = "catppuccin" },
             ["nelsyeung/twig.vim"] = {},
-            ["blueyed/smarty.vim"] = {},
-        },
-    },
-    lsp = {
-        formatting = {
-            format_on_save = false,
+            ["blueyed/smarty.vim"] = {}
         }
     },
-    options = {
-        opt = {
-            wrap = true,
-            textwidth = 90,
-        }
-    },
+    lsp = { formatting = { format_on_save = false } },
+    options = { opt = { wrap = true, textwidth = 80 } },
     mappings = {
         i = {
-        },
+            -- ["<C>Enter"] = {
+            --     function() require("copilot.suggestion").accept() end,
+            --     desc = "Accept copilot suggestion"
+            -- }
+        }
     }
 }
 
