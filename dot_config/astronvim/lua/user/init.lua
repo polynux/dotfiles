9abe89f8d542db6e0fe518f9121b29a2743da7df
Guillaume Dorce <polynux.dev@gmail.com>
2023-01-19 14:23:13 +0100

    add nvim surround

--- dot_config/astronvim/lua/user/init.lua
+++ dot_config/astronvim/lua/user/init.lua
@@ -8,7 +8,7 @@ local config = {
                     vim.defer_fn(function()
                         require("copilot").setup({
                             suggestion = { enabled = true, auto_trigger = true },
-                            panel = { enabled = false },
+                            panel = { enabled = false }
                             -- suggestion = { enabled = false },
                         })
                     end, 100)
@@ -18,18 +18,26 @@ local config = {
                 after = { "copilot.lua", "nvim-cmp" },
                 config = function()
                     require("copilot_cmp").setup({
-                        method = "getPanelCompletions",
+                        method = "getPanelCompletions"
                     })
-                    astronvim.add_user_cmp_source ("copilot", 800)
+                    astronvim.add_user_cmp_source("copilot", 800)
                 end
             },
             ["catppuccin/nvim"] = { as = "catppuccin" },
             ["nelsyeung/twig.vim"] = {},
-            ["blueyed/smarty.vim"] = {}
+            ["blueyed/smarty.vim"] = {},
+            ["kylechui/nvim-surround"] = {
+                tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+                config = function()
+                    require("nvim-surround").setup({
+                        -- Configuration here, or leave empty to use defaults
+                    })
+                end
+            }
         }
     },
     lsp = { formatting = { format_on_save = false } },
-    options = { opt = { wrap = true, textwidth = 80, colorcolumn = 80, } },
+    options = { opt = { wrap = true, textwidth = 80, colorcolumn = "80" } },
     mappings = {
         i = {
             -- ["<C>Enter"] = {
