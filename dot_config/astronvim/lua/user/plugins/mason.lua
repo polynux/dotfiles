b8baab16aeb522fc9739fd28188fde274ec8c9ab
Guillaume Dorce <polynux.dev@gmail.com>
2023-03-11 16:33:28 +0100

    mason ensure installed

---
+++ dot_config/astronvim/lua/user/plugins/mason.lua
@@ -0,0 +1,39 @@
+return {
+	"williamboman/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"cssls",
+			"html",
+			"intelephense",
+			"marksman",
+			"jsonls",
+			"sqls",
+			"lua_ls",
+			"tsserver",
+			"yamlls",
+		},
+	},
+}, {
+	"jay-babu/mason-null-ls.nvim",
+	opts = {
+		ensure_installed = {
+			"shellcheck",
+			"stylua",
+			"prettierd",
+			"shfmt",
+			"shellcheck",
+		},
+	},
+	config = function(_, opts)
+		local mason_null_ls = require("mason-null-ls")
+		local null_ls = require("null-ls")
+		mason_null_ls.setup(opts)
+		mason_null_ls.setup_handlers({
+			prettierd = function()
+				null_ls.register(
+					null_ls.builtins.formatting.prettierd.with({ extra_filetypes = { "markdown", "rmd", "qmd" } })
+				)
+			end,
+		})
+	end,
+}
