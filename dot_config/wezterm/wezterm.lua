c5ac17e5315c586965c6ace550b7b4e621864770
Guillaume Dorce <polynux.dev@gmail.com>
2023-03-11 15:02:28 +0100

    add wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font 'CaskaydiaCove Nerd Font Mono',
+  color_scheme = 'Catppuccin Mocha',
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
+}
+
