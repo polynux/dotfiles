70a2ad1ae0157ff1717f8aaf75eea9055f228b6f
Guillaume Dorce <guillaume.dorce@bm-services.com>
2024-05-15 15:58:42 +0200

    kickvim db ui

--- dot_config/kickvim/init.lua
+++ dot_config/kickvim/init.lua
@@ -11,7 +11,7 @@ local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     'git', 'clone', '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git', '--branch=stable',     -- latest stable release
+    'https://github.com/folke/lazy.nvim.git', '--branch=stable', -- latest stable release
     lazypath
   }
 end
@@ -57,7 +57,7 @@ require('lazy').setup({
             }
           }
         }
-      },       -- Additional lua configuration, makes nvim stuff amazing!
+      }, -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim'
     }
   }, {
@@ -68,10 +68,10 @@ require('lazy').setup({
     'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip',
 
     -- Adds LSP completion capabilities
-    'hrsh7th/cmp-nvim-lsp',         -- Adds a number of user-friendly snippets
+    'hrsh7th/cmp-nvim-lsp', -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets'
   }
-},     -- Useful plugin to show you pending keybinds.
+}, -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',   opts = {} }, {
   -- Adds git related signs to the gutter, as well as utilities for managing changes
   'lewis6991/gitsigns.nvim',
@@ -98,7 +98,7 @@ require('lazy').setup({
         { buffer = bufnr, desc = '[P]review [H]unk' })
     end
   }
-},     -- {
+}, -- {
   --   -- Theme inspired by Atom
   --   'navarasu/onedark.nvim',
   --   priority = 1000,
@@ -360,7 +360,7 @@ require('nvim-treesitter.configs').setup {
   textobjects = {
     select = {
       enable = true,
-      lookahead = true,       -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
         ['aa'] = '@parameter.outer',
@@ -373,7 +373,7 @@ require('nvim-treesitter.configs').setup {
     },
     move = {
       enable = true,
-      set_jumps = true,       -- whether to set jumps in the jumplist
+      set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
         [']m'] = '@function.outer',
         [']]'] = '@class.outer'
@@ -582,11 +582,19 @@ cmp.setup {
     end, { 'i', 's' })
   },
   sources = {
-    { name = 'nvim_lsp' }, { name = 'luasnip' }
+    { name = 'nvim_lsp' }, { name = 'luasnip' },
+    { name = 'buffer' }, { name = 'path' },
     -- Copilot Source
     -- { name = "copilot", group_index = 2 },
   }
 }
+
+cmp.setup.filetype({ "sql" }, {
+  sources = {
+    { name = "vim-dadbod-completion" },
+    { name = "buffer" },
+  },
+})
 
 -- if filetype is php, set tabstop to 4
 vim.api.nvim_create_autocmd("FileType", {
