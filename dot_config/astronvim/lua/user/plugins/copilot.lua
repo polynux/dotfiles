18d39c68fc54a428b6c44b8a720f988ddb24d9a5
Guillaume Dorce <guillaume.dorce@bm-services.com>
2024-06-10 12:45:46 +0200

    astronvim: laravel + copilot

--- dot_config/astronvim/lua/user/plugins/copilot.lua
+++ dot_config/astronvim/lua/user/plugins/copilot.lua
@@ -5,7 +5,11 @@ return {
     config = function()
         vim.defer_fn(function()
             require("copilot").setup({
-                suggestion = {enabled = true, auto_trigger = true}
+                suggestion = {enabled = true, auto_trigger = true},
+                filetypes = {
+                    yaml = true,
+                    markdown = true
+                }
             })
         end, 100)
     end
