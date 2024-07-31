4051cecd5b7afe3496375a45658b1a7deac13af6
Guillaume Dorce <polynux.dev@gmail.com>
2024-07-31 20:27:18 +0200

    add teelscope frecency

--- dot_config/kickvim/init.lua
+++ dot_config/kickvim/init.lua
@@ -326,8 +326,8 @@ vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers,
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files,
   { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files,
-  { desc = '[S]earch [F]iles' })
+-- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files,
+--   { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags,
   { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
@@ -336,6 +336,17 @@ vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep,
   { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics,
   { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set("n", "<Leader>tf", function()
+  require("telescope").extensions.frecency.frecency {}
+end)
+
+-- telescope frecency for current working directory
+vim.keymap.set("n", "<Leader>sf", function()
+  require("telescope").extensions.frecency.frecency {
+    workspace = "CWD",
+  }
+end, { desc = '[S]earch [F]recency (files)' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
